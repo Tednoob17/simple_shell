ee6dc0f2d8ba4a6ff4c379ad3da2062b1fb0a1a3
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:39:14 +0100

    next ->po9565

--- exits.c
+++ exits.c
@@ -18,3 +18,15 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
