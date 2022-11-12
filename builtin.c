33fb9a80180e42980eb9646f3c8dd4a4df6d7573
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:48:44 +0100

    buiom9

--- builtin.c
+++ builtin.c
@@ -6,8 +6,7 @@
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
- */
- 
+ */ 
 int _myexit(info_t *info)
 {
 	int exitcheck;
@@ -35,8 +34,7 @@ int _myexit(info_t *info)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
- */
- 
+ */ 
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
@@ -45,6 +43,7 @@ int _mycd(info_t *info)
 	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
+
 	if (!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
