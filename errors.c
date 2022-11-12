aaba924b64af75db033cf172072faabf96b1bec4
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:42:56 +0100

    motra

--- errors.c
+++ errors.c
@@ -40,3 +40,26 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
+/**
+ * _putfd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
