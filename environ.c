517933ab99639816643061526fcf00e0241da380
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:32:47 +0100

    4l

--- environ.c
+++ environ.c
@@ -19,3 +19,17 @@ int _myenv(info_t *info)
  *
  * Return: the value
  */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
