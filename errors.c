c6ee97c673374db2bdf1d527f654ffec81e72c84
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:42:19 +0100

    motr

--- errors.c
+++ errors.c
@@ -26,3 +26,17 @@ void _eputs(char *str)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
