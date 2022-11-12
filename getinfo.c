6666a3c0c76ae6c8e17e921c000b65cd9456b8dc
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:01:19 +0100

    chrom

--- getinfo.c
+++ getinfo.c
@@ -4,3 +4,10 @@
  * clear_info - initializes info_t struct
  * @info: struct address
  */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
