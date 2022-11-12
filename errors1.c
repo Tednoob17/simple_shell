335710b6cfc6a68b5fb90646540d662440fb1ed4
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:49:25 +0100

    addrs

--- errors1.c
+++ errors1.c
@@ -91,3 +91,30 @@ int print_d(int input, int fd)
  *
  * Return: string
  */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
