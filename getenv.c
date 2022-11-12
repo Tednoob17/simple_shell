006e605a06bd83ef15d4def5099fe105c1a5fde0
Tednoob17 <teddams047@gmail.com>
2022-11-12 03:22:21 +0100

    lo65

--- getenv.c
+++ getenv.c
@@ -16,3 +16,10 @@ char **get_environ(info_t *info)
 
 	return (info->environ);
 }
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
