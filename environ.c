fd935befc9f419ec372d6e91846edee175a9db33
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:51:45 +0100

    boom42

--- environ.c
+++ environ.c
@@ -78,7 +78,7 @@ int _myunsetenv(info_t *info)
  *          constant function prototype.
  * Return: Always 0
  */
- int populate_env_list(info_t *info)
+int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
 	size_t i;
