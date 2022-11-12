9f1c35c9e6a9fef91cb5854586ad2ec9f743e238
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:50:38 +0100

    addrs72

--- errors1.c
+++ errors1.c
@@ -124,3 +124,14 @@ char *convert_number(long int num, int base, int flags)
  *
  * Return: Always 0;
  */
+ void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
