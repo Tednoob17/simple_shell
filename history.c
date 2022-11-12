88c95ea975aeae9c72817d141ac0a6a8b90f8f69
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:54:04 +0100

    addrs7269

--- history.c
+++ history.c
@@ -6,3 +6,20 @@
  *
  * Return: allocated string containg history file
  */
+char *get_history_file(info_t *info)
+{
+	char *buf, *dir;
+
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
+}
+
