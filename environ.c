9b15953f1716d2a777f8e9c52e065f4a2f821c65
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:34:24 +0100

    4loopb

--- environ.c
+++ environ.c
@@ -57,3 +57,17 @@ int _mysetenv(info_t *info)
  *        constant function prototype.
  *  Return: Always 0
  */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
