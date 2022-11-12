65a1445190bb8a25ace07f1986df1ee492f583ba
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:04:41 +0100

    chrome658pr

--- getLine.c
+++ getLine.c
@@ -43,4 +43,9 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	}
 	return (r);
 }
-
+/**
+ * get_input - gets a line minus the newline
+ * @info: parameter struct
+ *
+ * Return: bytes read
+ */
