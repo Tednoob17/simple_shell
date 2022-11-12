d6798d251c217c046ed2f9bb1b4a835992c5e42b
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:48:07 +0100

    addsap8

--- errors1.c
+++ errors1.c
@@ -52,3 +52,34 @@ void print_error(info_t *info, char *estr)
  *
  * Return: number of characters printed
  */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
