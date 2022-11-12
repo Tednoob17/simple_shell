52e6af56fbdd3a3c1b0cb1a040648c29f6092bd3
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:30:08 +0100

    42

---
+++ environ.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
