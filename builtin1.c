c618b577c025910849a1c56e73b7cf8c4dbc5ffb
Tednoob17 <teddams047@gmail.com>
2022-11-12 02:24:44 +0100

    crack youm

--- builtin1.c
+++ builtin1.c
@@ -26,3 +26,12 @@ int unset_alias(info_t *info, char *str)
 
 	p = _strchr(str, '=');
 	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
+
