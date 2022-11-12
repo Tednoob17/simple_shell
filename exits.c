bf81ee9f1c34a6e7736f3819994b0a28c97eb44c
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:40:49 +0100

    faust 2?

--- exits.c
+++ exits.c
@@ -45,4 +45,15 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
-	
+			i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
