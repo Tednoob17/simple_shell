8021ab930846ed16b42b7d8dd37e131735417ce0
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:02:13 +0100

    chrome6

--- getinfo.c
+++ getinfo.c
@@ -17,3 +17,30 @@ void clear_info(info_t *info)
  * @info: struct address
  * @av: argument vector
  */
+void set_info(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
