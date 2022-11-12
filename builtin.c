11bedbdb37ff837715e1a8660aa3405dc393fc94
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:18:59 +0100

    bump189

--- builtin.c
+++ builtin.c
@@ -1,1 +1,22 @@
 #include "shell.h"
+/**
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+		info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
