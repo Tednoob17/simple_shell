9730858070379f71633e7e85db48e51fc8275d8b
Tednoob17 <teddams047@gmail.com>
2022-11-12 03:25:05 +0100

    lo654

--- getenv.c
+++ getenv.c
@@ -46,3 +46,13 @@ int _unsetenv(info_t *info, char *var)
 	}
 	return (info->env_changed);
 }
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
