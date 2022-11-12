3d1d4d5e1346a291ea3ef1e06375da736858c2d5
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:21:22 +0100

    bump5555

--- builtin.c
+++ builtin.c
@@ -27,3 +27,9 @@ int _myexit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
+/**
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
