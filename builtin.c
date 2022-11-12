5c34a83f2c879fec49855cab3a2742e910326ec7
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:23:10 +0100

    bump589

--- builtin.c
+++ builtin.c
@@ -37,3 +37,9 @@ int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
+	
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	
