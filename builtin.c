9dbeeb05f72676179409196c4bf61b1f52c18587
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:28:14 +0100

    bump989

--- builtin.c
+++ builtin.c
@@ -58,7 +58,12 @@ int _mycd(info_t *info)
 			_putchar('\n');
 			return (1);
 		}
-		
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
 	
 	
 	
