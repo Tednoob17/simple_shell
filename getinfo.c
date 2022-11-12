79b935eb9552ab3841d7ca66b426fa7ce218fdc9
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:02:37 +0100

    chrome65

--- getinfo.c
+++ getinfo.c
@@ -43,4 +43,31 @@ void set_info(info_t *info, char **av)
 		replace_vars(info);
 	}
 }
-
+/**
+ * free_info - frees info_t struct fields
+ * @info: struct address
+ * @all: true if freeing all fields
+ */
+void free_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+			info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
