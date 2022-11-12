29af0aaa5fce894e9858ed76d8575bfb95b3908e
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:03:29 +0100

    chrome658

---
+++ getLine.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+
+/**
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
