052c06f724bbfeb6ada1dde9b608c1ef66bfb2b9
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:56:28 +0100

    charbeyyy86

--- history.c
+++ history.c
@@ -108,3 +108,16 @@ int read_history(info_t *info)
  *
  * Return: Always 0
  */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+
