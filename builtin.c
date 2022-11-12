bb6a97adce08d211bc42bbe99d95b12778ada156
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:33:03 +0100

    nemiroo4

--- builtin.c
+++ builtin.c
@@ -79,6 +79,7 @@ int _mycd(info_t *info)
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
+  *          constant function prototype.
 	
 	
 	
