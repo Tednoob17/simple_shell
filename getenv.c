e00c989728af8b34316a54153d451d6a66546c5b
Tednoob17 <teddams047@gmail.com>
2022-11-12 03:21:00 +0100

    lolmp755

--- getenv.c
+++ getenv.c
@@ -8,3 +8,11 @@
  */
 char **get_environ(info_t *info)
 {
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
