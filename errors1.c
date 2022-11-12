d8a549c9d8246e53824d878177a4ea0fecc6bfea
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:45:20 +0100

    add

--- errors1.c
+++ errors1.c
@@ -6,3 +6,25 @@
  * Return: 0 if no numbers in string, converted number otherwise
  *       -1 on error
  */
+ int _erratoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
