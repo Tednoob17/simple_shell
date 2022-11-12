5c5de7bfd97795e8acdb708864f45c095cc68b6e
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:32:22 +0100

    4258

--- environ.c
+++ environ.c
@@ -11,3 +11,11 @@ int _myenv(info_t *info)
 	print_list_str(info->env);
 	return (0);
 }
+
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
