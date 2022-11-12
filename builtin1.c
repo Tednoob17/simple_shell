151c555c499a963e70c5bb7fd57d81ed1b85538c
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:31:57 +0100

    next ->4

--- builtin1.c
+++ builtin1.c
@@ -83,3 +83,19 @@ int set_alias(info_t *info, char *str)
  *          constant function prototype.
  *  Return: Always 0
  */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
