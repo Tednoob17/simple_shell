45ca28e9ec07152e6fe0e4ba55e6bd5c2d70a5df
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:56:55 +0100

    ch

--- history.c
+++ history.c
@@ -121,3 +121,21 @@ int build_history_list(info_t *info, char *buf, int linecount)
 	return (0);
 }
 
+/**
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
+int renumber_history(info_t *info)
+{
+	list_t *node = info->history;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
+}
