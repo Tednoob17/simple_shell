239db14b8a041044b237b98cd7967303137eed20
Tednoob17 <teddams047@gmail.com>
2022-11-12 11:38:01 +0100

    next ->po9

--- exits.c
+++ exits.c
@@ -3,6 +3,7 @@
 /**
  **_strncpy - copies a string
   *@dest: the destination string to be copied to
-   *@src: the source string
-   *@n: the amount of characters to be copied
-   *Return: the concatenated string
+  *@src: the source string
+  *@n: the amount of characters to be copied
+  *Return: the concatenated string
+  */
