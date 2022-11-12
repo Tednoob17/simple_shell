1d1e4e40892cd5b8c22cd939592c7cbc206a1fa0
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:00:06 +0100

    bump56

--- _atoi.c
+++ _atoi.c
@@ -39,7 +39,12 @@ int _isalpha(int c)
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
- int _atoi(char *s)
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+
  
  
  
