deb0590212a6aef0f7394617565ca2a08bcd70bb
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:55:22 +0100

    charbeyyy

--- history.c
+++ history.c
@@ -82,4 +82,21 @@ int read_history(info_t *info)
 	buf[fsize] = 0;
 	if (rdlen <= 0)
 		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
+}
 
