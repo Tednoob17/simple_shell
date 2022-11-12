4b6880a0ebc393f80efa1473a338852db61d3a6d
Tednoob17 <teddams047@gmail.com>
2022-11-12 03:19:36 +0100

    lolmp7

--- getenv.c
+++ getenv.c
@@ -1,2 +1,8 @@
 #include "shell.h"
 
+/**
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
