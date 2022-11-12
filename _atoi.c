281cff2dffaab4788e213ea2789006a0244ff354
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:00:55 +0100

    bump569

--- _atoi.c
+++ _atoi.c
@@ -44,6 +44,11 @@ int _atoi(char *s)
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
 
  
  
