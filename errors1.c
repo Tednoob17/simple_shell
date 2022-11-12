0bce4e1e08fbce6b02b6b1bddeba9c0d93fa91ae
Tednoob17 <teddams047@gmail.com>
2022-11-12 10:44:46 +0100

    motrap55

---
+++ errors1.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+
+/**
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
+ */
