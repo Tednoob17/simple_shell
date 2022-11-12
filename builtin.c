5fa68148bbe9cab24b376058ee8728fab9cbb329
Tednoob17 <teddams047@gmail.com>
2022-11-12 01:29:03 +0100

    bump69

--- builtin.c
+++ builtin.c
@@ -66,6 +66,9 @@ int _mycd(info_t *info)
 		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
 	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
 	
 	
 	
